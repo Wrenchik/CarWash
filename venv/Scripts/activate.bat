23c3f905909fcc54fb35b2d655dc2bf58fee2c34
platunova-ann <aplatunovasergeeva@gmail.com>
2025-04-21 15:29:15 +0500

    21.04 css по первому шаблону(только часть)

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=M:\Dev\CarWash\venv
+set VIRTUAL_ENV=C:\Users\aanuu\IdeaProjects\CarWash\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
