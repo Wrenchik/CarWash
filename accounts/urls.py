ecc2f8e2690be8708d1a7fde6d19803009f58b16
MidoriKsai <minoginadara@gmail.com>
2025-04-30 18:20:23 +0500

    Css

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('profile/', CustomLogoutView.as_view(), name='profile'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from .models import Service
+from .models import Service, Review
 
 
 def get_main_page(request):
     template_name = 'main/main.html'
 
     services = Service.objects.all()
+    reviews = Review.objects.filter(is_published=True).order_by('-date')[:5]
 
     return render(request, template_name,  {'services': services})
